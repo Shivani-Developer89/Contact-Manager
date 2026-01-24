d7ee4cec2d48692430ecc777ef0d6839b2172a2b
Shivani-Developer89 <shivani25782@gmail.com>
2026-01-25 02:07:09 +0530

    Initial commit

---
+++ src/main/java/com/scm/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/scm")
+public class PageController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home Page Handler");
+        model.addAttribute("name","Springboot Application");
+        model.addAttribute("YoutubeChannel","SpringIO");
+        model.addAttribute("GithubRepo","https://github.com/Shivani-Developer89");
+        return "home";
+    }
+}
