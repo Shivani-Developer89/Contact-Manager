9c0fd53ab76eb14bba26416e76fe73db3b36f538
Shivani-Developer89 <shivani25782@gmail.com>
2026-03-29 23:47:12 +0530

    Add and update features

--- src/main/resources/static/Js/script.js
+++ src/main/resources/static/Js/script.js
@@ -1,1 +1,21 @@
 console.log("Script loaded");
+
+let currentTheme = getTheme();
+changeTheme();
+
+//TODO
+function changeTheme() {
+    document.querySelector("html").classList.add(currentTheme)
+
+}
+
+//set theme to localStorage
+function setTheme(theme) {
+    localStorage.setItem("theme ", theme)
+}
+
+// get theme from lacalStorage
+function getTheme() {
+    let theme = localStorage.getItem("theme");
+    return theme ? theme : "light";
+}
