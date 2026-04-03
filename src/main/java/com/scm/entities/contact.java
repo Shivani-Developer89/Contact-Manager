db7a1ebe211f687e6a37416cf54ca9daf900881b
Shivani-Developer89 <shivani25782@gmail.com>
2026-04-03 20:53:18 +0530

    correct the entity

--- src/main/java/com/scm/entities/contact.java
+++ src/main/java/com/scm/entities/contact.java
@@ -1,10 +1,18 @@
-package com.scm.entities;
 
+package com.scm.entities;
+import java .util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
-public class contact {
+@Entity
+public class Contact {
     @Id
     private String id;
     private String name;
@@ -20,4 +28,9 @@ public class contact {
     @ManyToOne
     private User user;
 
+    @OneToMany(mappedBy = "contact",cascade=CascadeType.ALL,fetch = FetchType.EAGER,orphanRemoval = true)
+    private List<socialLink> Links = new ArrayList<>();
 }
+
+
+
