fbfe28ccf402d8024a5c0655840bc315dff86f54
Shivani-Developer89 <shivani25782@gmail.com>
2026-04-14 23:42:09 +0530

    Giving new anotation that helps to fetch data

--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,16 +1,18 @@
 package com.scm.forms;
 
-import com.scm.entities.Lombok;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Lombok
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
+@ToString
 public class UserForm {
     private String name;
     private String  email;
